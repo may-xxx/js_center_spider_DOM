2146118d3073ebf2c3aa11a8d01b989becab8973
May <miaqu138@gmail.com>
2024-08-24 18:47:41 +0200

    make build

---
+++ index.2cd14cca.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall").getBoundingClientRect(),i=e.getBoundingClientRect(),l=t.width/2-(i.width/2+10),n=t.height/2-(i.height/2+10);e.style.left=l+"px",e.style.top=n+"px";
+//# sourceMappingURL=index.2cd14cca.js.map
